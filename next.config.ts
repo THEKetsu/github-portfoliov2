41e64dbc5c93870020845a2c9ae74e92196c355d
THEKetsu <bender.quent@gmail.com>
2024-11-13 07:19:56 +0100

    Try to debug image

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    unoptimized: true, // This disables image optimization for GitHub Pages
+  },
+  // Add other Next.js config options here as needed
 };
 
 export default nextConfig;
